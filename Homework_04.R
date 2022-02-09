86ed92d58848be843cfe9727adb1e546fe610be9
LindseyCarlson8 <lindseycarlson8@gmail.com>
2022-02-09 14:59:04 -0500

    created and added homework 4

---
+++ Homework_04.R
@@ -0,0 +1,51 @@
+# Computational Biology Homework 4
+# 9 February 2022
+# LSC 
+
+#Class Notes - Vector Review
+  # type numeric/integer/double
+a <- c(10:30)
+
+  #type character
+b <- c("Carrots", "Lettuce","Tomatoes")
+
+  #binary/Boolean/Logical
+c <- c(TRUE, TRUE, FALSE)
+c <- C(T, T, F)
+
+
+
+
+
+# Question 1 
+x<-1.1
+a<-2.2
+b<-3.3
+
+# 1a
+z <- x^(a^b)
+print(z)
+
+#1b
+z <- (x^a)^b
+print(z)
+
+#1c
+z <- 3(x^3) + 2(x^2) + 1
+print(z)
+
+
+#Question 2 
+#2a
+Q2a <- c((seq(from=1, to = 8, by= )),(seq(from=7, to=1, by=-1)))
+print(Q2a)
+
+#2b
+Q2b <- c(rep(1,1), rep(2,2), rep(3,3), rep(4,4), rep(5,5))
+print(Q2b)
+
+#2c
+Q2c <- c(rep(5,1), rep(4,2), rep(3,3), rep(2,4), rep(1,5))
+print(Q2c)
+
+#Question 3
